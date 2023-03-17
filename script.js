e3dbfc16ce8b3139282f3b601b39061836d2c17f
CommanderBALA <bala.pasztor.g@gmail.com>
2023-03-17 20:22:58 +0100

    upload files

---
+++ script.js
@@ -0,0 +1,176 @@
+let szam = document.getElementById("szam")
+let muv = document.getElementById("muvelet")
+let err = document.getElementById("err")
+let pb = document.getElementById("pb")
+let tim = document.getElementById("time")
+let endTime = document.getElementById("endTime")
+
+let ö_k = ["+", "-"]
+let sz_o = ["*", "/"]
+let minden = ["+", "-", "*", "/"]
+
+let min = 0
+let sec = 0
+let time = false
+let end = false
+
+function Check() {
+    if (end) return;
+    let goods = []
+    for (let i = 1; i < 11; i++) {
+        let a = document.getElementById(`a${i}`)
+        let b = document.getElementById(`b${i}`)
+        let inp = document.getElementById(`inp${i}`)
+        let res = document.getElementById(`res${i}`)
+        let jel = document.getElementById(`jel${i}`) 
+        let muvelet = muv.value
+
+        if (muvelet == "ö+k" || muvelet == "sz+o" || muvelet == "m") {
+            muvelet = jel.innerHTML
+        }
+
+        if (muvelet == "-") {
+            if (parseInt(inp.value) == parseInt(a.innerHTML) - parseInt(b.innerHTML)) {
+                res.innerHTML = "✅"
+            } else {
+                res.innerHTML = "❌"
+            }
+        } else if (muvelet == "+") {
+            if (parseInt(inp.value) == parseInt(a.innerHTML) + parseInt(b.innerHTML)) {
+                res.innerHTML = "✅"
+            } else {
+                res.innerHTML = "❌"
+            }
+        } else if (muvelet == "*") {
+            if (parseInt(inp.value) == parseInt(a.innerHTML) * parseInt(b.innerHTML)) {
+                res.innerHTML = "✅"
+            } else {
+                res.innerHTML = "❌"
+            }
+        } else if (muvelet == "/") {
+            if (parseInt(inp.value) == parseInt(a.innerHTML) / parseInt(b.innerHTML)) {
+                res.innerHTML = "✅"
+            } else {
+                res.innerHTML = "❌"
+            }
+        }
+
+    }
+
+    for (let i = 1; i < 11; i++) {
+        let res = document.getElementById(`res${i}`)
+        if (res.innerHTML == "✅") {
+            goods.push(i)
+        }
+    }
+
+    let res = document.getElementById("res")
+
+    if (goods.length == 10) {
+        res.innerHTML = "✅"
+        time = false
+        end = true
+        endTime.innerHTML = tim.innerHTML
+    } else {
+        res.innerHTML = "❌"
+    }
+
+    goods = []
+}
+
+function Start() {
+    let max = 0
+    let muvelet = muv.value
+    if (szam.value == "def" && muvelet == "def") {
+        err.innerHTML = "Nincs megadva a művelet és a számkészlet"
+        err.style.display = "block"
+        err.style.opacity = 1
+
+        return;
+    } else if (szam.value == "def") {
+        err.innerHTML = "Nincs megadva a számkészlet"
+        err.style.display = "block"
+        err.style.opacity = 1
+
+        return;
+    } else if (muvelet == "def") {
+        err.innerHTML = "Nincs megadva a művelet"
+        err.style.display = "block"
+        err.style.opacity = 1
+
+        return;
+    }
+
+    max = szam.value
+    let muve = ""
+    for (let i = 1; i < 11; i++) {
+        let sz1 = Math.floor(Math.random() * max);
+        let sz2 = Math.floor(Math.random() * max);
+        let a = document.getElementById(`a${i}`)
+        let b = document.getElementById(`b${i}`)
+        let jel = document.getElementById(`jel${i}`) 
+
+        if (muvelet == "ö+k") muve = ö_k[Math.floor((Math.random()*ö_k.length))]
+        else if (muvelet == "sz+o") muve = sz_o[Math.floor((Math.random()*sz_o.length))]
+        else if (muvelet == "m") muve = minden[Math.floor((Math.random()*minden.length))]
+        else muve = muvelet
+
+        jel.innerHTML = muve
+
+        if (muve == "-") {
+            while (sz1 < sz2) {
+                sz1 = Math.floor(Math.random() * max);
+                sz2 = Math.floor(Math.random() * max);
+            }
+            a.innerHTML = sz1
+            b.innerHTML = sz2
+        } else if (muve == "/") {
+            let remainder = sz1 % sz2
+            while (remainder != 0) {
+                sz1 = Math.floor(Math.random() * max);
+                sz2 = Math.floor(Math.random() * max);
+                remainder = sz1 % sz2
+            }
+            a.innerHTML = sz1
+            b.innerHTML = sz2
+        } else {
+            a.innerHTML = sz1
+            b.innerHTML = sz2
+        }
+    }
+    time = true
+    end = false
+    pb.style.display = "none"
+    Time()
+}
+
+let oldMin = 0
+let oldSec = 0
+
+function Time() {
+    if (!time) {
+        return;
+    }
+
+    sec = oldSec
+    min = oldMin
+
+    sec += 1
+
+    if (sec == 60) {
+        sec = 0
+        min += 1
+    }
+
+    oldSec = sec
+    oldMin = min
+
+    if (min < 10) min = `0${min}`;
+    if (sec < 10) sec = `0${sec}`;
+
+    let timeNum = min + ":" + sec
+
+    tim.innerHTML = timeNum
+
+    setTimeout(() => {Time()}, 1000)
+}
